56d9b9f8d4e7caf238124bae82dc29971dddfe5f
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-12-15 09:02:22 -0600

    Refactor create_settings_file module location.
    Moved create_settings_file to the utilities directory for better organization. Updated imports accordingly to reflect the new location. Added address_groups to the module imports for improved object handling.

--- scm_config_clone/__init__.py
+++ scm_config_clone/__init__.py
@@ -1,5 +1,6 @@
 # scm_config_clone/__init__.py
 
-from .commands.create_settings_file import create_settings
 from .commands.objects.address import addresses
+from .commands.objects.address_groups import address_groups
 from .commands.objects.tag import tags
+from .commands.utilities.create_settings_file import create_settings
